5883cfa0731197d9172f143cfe2b30640901039b
vitorfail <vitor_andrademanoel@hotmail.com>
2023-03-26 16:27:54 -0300

    teclas

--- src/App.js
+++ src/App.js
@@ -6,11 +6,29 @@ function App() {
         <div className='tela'>
             <label>0,00</label>
         </div>
-        <div className='teclado'>
-          <div className='tecla'>1</div>
-          <div className='tecla'>2</div>
-          <div className='tecla'>3</div>
-          <div className='tecla'>4</div>
+        <div className='quadro'>
+          <div className='teclado'>
+            <div className='tecla'>1</div>
+            <div className='tecla'>2</div>
+            <div className='tecla'>3</div>
+            <div className='tecla'>P1</div>
+            <div className='tecla'>4</div>
+            <div className='tecla'>5</div>
+            <div className='tecla'>6</div>
+            <div className='tecla'>P2</div>
+            <div className='tecla'>7</div>
+            <div className='tecla'>8</div>
+            <div className='tecla'>9</div>
+            <div className='tecla'>$</div>
+            <div className='tecla'>C</div>
+            <div className='tecla'>0</div>
+            <div className='tecla'>E</div>
+            <div className='tecla'>V</div>
+            <div className='tecla'>G1</div>
+            <div className='tecla'>◀</div>
+            <div className='tecla'>▶</div>
+            <div className='tecla'>G2</div>
+          </div>
         </div>
     </div>
   );
