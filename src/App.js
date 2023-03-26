f4aa728b68f254079b02ca380ff7083a6210c81c
vitorfail <vitor_andrademanoel@hotmail.com>
2023-03-26 14:43:16 -0300

    initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className='tela'>
+            <label>0,00</label>
+        </div>
+        <div className='teclado'>
+          <div className='tecla'>1</div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
