83c60bd27c4ffe9f5409b988fa77bebb867a96b7
vitorfail <vitor_andrademanoel@hotmail.com>
2023-03-28 14:13:44 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,35 +1,68 @@
 import './App.css';
 import { useState } from 'react';
-import Logo from "./gilbarco.png" 
-
+import Logo from "./gilbarco.png";
+import At from "./at.png";
+import Mangeuria from "./mang.png"
 
 function App() {
   const [valor, setvalor] = useState("0,00")
   const [volume, setvolume] = useState("0,00")
-  const [troca, settroca] = useState(true)
+  const [ direita, setdireita] = useState(0)
+  const [modal_moeda, setmodal_moeda] = useState(false)
+  const [modal_seta, setmodal_seta] = useState(false)
+  const [troca, settroca] = useState(0)
   function adicionar(tecla){
-    if(troca){
-      tecla = String(tecla)
-      if(valor === "0,00"){
-        setvalor("")
-      }
-      if(tecla !== "Gum" || tecla !== "Gdois" || tecla !== "Pum" || tecla !== "Pdois" || tecla !== "$" || tecla !== "C" || tecla !== "E"){
-        setvalor( mascara_valor(valor + (tecla)))
+    if(direita !==0){
+      if(troca !== 0){
+        if(troca === 1){
+          tecla = String(tecla)
+          if(valor === "0,00"){
+            setvalor("")
+          }
+          if(tecla !== "Gum" || tecla !== "Gdois" || tecla !== "Pum" || tecla !== "Pdois" || tecla !== "$" || tecla !== "C" || tecla !== "E"){
+            setvalor( mascara_valor(valor + (tecla)))
+          }
+          else{
+            if(tecla === "Pum"){
+                setvalor("5,00")
+            }
+          }  
+        }
+        else{
+          tecla = String(tecla)
+          if(volume === "0,00"){
+            setvolume("")
+          }
+          if(tecla !== "Gum" || tecla !== "Gdois" || tecla !== "Pum" || tecla !== "Pdois" || tecla !== "$" || tecla !== "C" || tecla !== "E"){
+            setvolume( mascara_valor(volume + (tecla)))
+          }
+          else{
+            if(tecla === "Pum"){
+                setvolume("5,00")
+            }
+          }  
+        }
       }
       else{
-        console.log("passou aqui")
-        if(tecla === "Pum"){
-            setvalor("5,00")
-        }
-      }    
+        setmodal_moeda(true)
+      }
     }
     else{
-      
+      setmodal_seta(true)
     }
   }
-  function volume(){
+  function escolher_combustivel(val){
+    if(valor ==="0,00"){
+      setdireita(val)
+    }
+  }
+  function moeda(){
     setvalor("0,00")
-    settroca(false)
+    settroca(1)
+  }
+  function vol(){
+    setvalor("0,00")
+    settroca(2)
   }
   function mascara_valor(e){
     e = e.replace(/\D/g, "")
@@ -42,8 +75,33 @@ function App() {
       return e
     }
   }
+
+  function abastecer(){
+    var abastecimento = parseFloat((valor.replace(".", "")).replace(',', "."))
+    var intervalo = abastecimento/15
+    setvalor("0,00")
+    setvalor(volume)
+    alert('Abastecido')
+  }
   return (
     <div className="App">
+        <div className='abastecer' onClick={() => abastecer()}>
+          <img src={Mangeuria} alt='abastecer'/>
+        </div>
+        <div className={modal_moeda?'modal-moeda show':'modal-moeda'}>
+          <div className='alerta'>
+            <img src={At} alt='atencao'/>
+            <p>Escolha se o valor digitado será em reais($) ou volume(VOL)</p>
+            <button onClick={() => setmodal_moeda(false)}>OK</button>
+          </div>
+        </div>
+        <div className={modal_seta?'modal-seta show':'modal-seta'}>
+          <div className='alerta'>
+            <img src={At} alt='atencao'/>
+            <p>Escolha qual lado da bomba será usado ◀ ▶</p>
+            <button onClick={() => setmodal_seta(false)}>OK</button>
+          </div>
+        </div>
         <div className='tela'>
             <label>{valor}</label>
         </div>
@@ -64,14 +122,14 @@ function App() {
             <div onClick={(event) => adicionar(event.target.innerText)} className='tecla'>7</div>
             <div onClick={(event) => adicionar(event.target.innerText)} className='tecla'>8</div>
             <div onClick={(event) => adicionar(event.target.innerText)} className='tecla'>9</div>
-            <div onClick={(event) => adicionar(event.target.innerText)} className='tecla'>$</div>
+            <div onClick={(event) => moeda()} className='tecla'>$</div>
             <div onClick={(event) => adicionar(event.target.innerText)} className='tecla'>C</div>
             <div onClick={(event) => adicionar(event.target.innerText)} className='tecla'>0</div>
             <div onClick={(event) => adicionar(event.target.innerText)} className='tecla'>E</div>
-            <div onClick={(event) => adicionar(event.target.innerText)} className='tecla'>V</div>
+            <div onClick={(event) => vol()} className='tecla'>V</div>
             <div onClick={(event) => adicionar(event.target.id)}  id="Gum" className='tecla'>G1</div>
-            <div onClick={(event) => adicionar(event.target.innerText)} className='tecla'>◀</div>
-            <div onClick={(event) => adicionar(event.target.innerText)} className='tecla'>▶</div>
+            <div onClick={() => escolher_combustivel(1)} className='tecla'>◀</div>
+            <div onClick={() => escolher_combustivel(2)} className='tecla'>▶</div>
             <div onClick={(event) => adicionar(event.target.id)}  id="Gdois" className='tecla'>G2</div>
           </div>
         </div>
